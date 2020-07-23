bd8c049439e23a4f92aa7a6e886708f7800691d2
mbhadange <65362340+mbhadange@users.noreply.github.com>
2020-07-23 13:47:25 -0400

    uses backend to load translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,26 +1,19 @@
 import i18n from "i18next";
 import { reactI18nextModule } from "react-i18next";
 
-import translationEN from './en/translation.json';
-import translationDE from './de/translation.json';
+// imports the translations from the "./public/locales/" folder
+import backend from "i18next-xhr-backend";
+
+//import translationEN from './en/translation.json';
+//import translationDE from './de/translation.json';
 
 import detector from "i18next-browser-languagedetector";
 
-// the translations that we would like to make
-const resources = {
-    en: {
-      translation: translationEN
-    },
-    de: {
-      translation: translationDE
-    }
-};
-
 i18n
   .use(detector)
+  .use(backend)
   .use(reactI18nextModule) // passes i18n to react-i18next
   .init({
-    resources,
     lng: "en",
     fallbackLng: "en", // if detected lng is not usable at the moment, then falls back on en
 
@@ -28,6 +21,10 @@ i18n
 
     interpolation: {
       escapeValue: false // react already safes from xss
+    },
+
+    react: {
+      wait: true
     }
   });
 
