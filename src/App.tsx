ced780501466252e2a65c9eeda543efc93ef174b
Daniel Ritchie <dritchie1031@gmail.com>
2020-08-03 18:18:01 -0700

    changed to single translation file

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 function HeaderComponent() {
   const [t, i18n] = useTranslation();
   return <div>
-    <h1>{t('welcome_other')}</h1>
+    <h1>{t('welcome.title', { framework: 'React' })}</h1>
     <button onClick={() => i18n.changeLanguage('de')}>de</button>
     <button onClick={() => i18n.changeLanguage('en')}>en</button>
   </div>
--- src/i18n.js
+++ src/i18n.js
@@ -3,20 +3,15 @@ import { initReactI18next } from "react-i18next";
 
 //import translationEN from './en/translation.json';
 //import translationDE from './de/translation.json';
-import common_de from './translations/de/App.De.json';
-import common_en from './translations/en/App.En.json';
+import trans from './AppTrans.json';
 
 i18n
   .use(initReactI18next) // passes i18n to react-i18next
   .init({
-    resources: {
-      en: {
-        translation: common_en
-      }
-    },
+    resources: trans,
     lng: "en",
 
-    keySeparator: false, // we do not use keys in form messages.welcome
+    keySeparator: '.', // we do not use keys in form messages.welcome
 
     interpolation: {
       escapeValue: false // react already safes from xss
